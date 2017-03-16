7720bc89fe945503db87951f2dd4541cc220a00f
helogargon <helogargon@gmail.com>
2017-03-16 12:09:48 +0100

    Actualizado contenido html

---
+++ fincasSD/src/main/resources/static/js/javascript.js
@@ -0,0 +1,23 @@
+$(function () {
+    $('.watchIt').click(function () {
+        $('.esconderTodo').css("visibility","visible");
+    });
+
+    $('.modifyButton').click(function () {
+        $('.input').removeAttr("readonly");
+        $('.rightAlign').css("visibility","visible");
+        $('.listaPropiedades').removeAttr("href");
+        $('.bottonSavePos').css("visibility","visible");
+    });
+
+    $('.bottonSavePos').click(function () {
+        $('.input').attr("readonly","readonly");
+        $('.rightAlign').css("visibility","hidden");
+        $('.listaPropiedades').attr("href","services-properties.html");
+        $('.bottonSavePos').css("visibility","hidden");
+    });
+
+    $('.rightAlign').click(function () {
+        $(this).parent().remove();
+    });
+});
