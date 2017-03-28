9bc8af3b270c0b2af6c28a58514e6479d711d5ca
helogargon <helogargon@gmail.com>
2017-03-28 14:43:29 +0200

    Cambiado añadir

--- fincasSD/src/main/java/com/sd/practica1/IndexController.java
+++ fincasSD/src/main/java/com/sd/practica1/IndexController.java
@@ -1,5 +1,7 @@
 package com.sd.practica1;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -58,8 +60,21 @@ public class IndexController {
 		
 		return "aniadir";
 	}
+	@RequestMapping(value="/aniadirPropiedad")
+	public String aniadirPropiedadController(Model model){
+		List<ComunidadDeVecinos> c= comunidadDeVecinosRepository.findAll();
+		List<Propietario> p= propietarioRepository.findAll();
+		model.addAttribute("comunities",c);
+		model.addAttribute("propietaries",p);
+		return "aniadirPropiedad";
+	}
+	@RequestMapping(value="/aniadirPropietario")
+	public String aniadirPropietarioController(Model model){
+		
+		return "aniadirPropietario";
+	}
 	
-	@RequestMapping(value="/uploadComunidad", method=RequestMethod.POST)
+	/**@RequestMapping(value="/uploadComunidad", method=RequestMethod.POST)
 	public String addComunidad(Model model, @RequestParam("cif") String cif, @RequestParam("poblacion") String poblacion, @RequestParam("calle") String calle, @RequestParam("numero") int numero, 
 			@RequestParam("codigoPostal") int codigoPostal, @RequestParam("numCuenta") String numCuenta, @RequestParam("portalPropiedad") String portalPropiedad, @RequestParam("planta") String planta, 
 			@RequestParam("letra") char letra, @RequestParam("nombreProp") String nombreProp, @RequestParam("apellidosProp") String apellidosProp, @RequestParam("dni") String dni, 
@@ -81,6 +96,37 @@ public class IndexController {
 		model.addAttribute("Direccion", dir);
 		model.addAttribute("Comunidad", cv);
 		return "mostrar";
+	}**/
+	@RequestMapping(value="/uploadComunidad", method=RequestMethod.POST)
+	public String addComunidad(Model model, @RequestParam("cif") String cif, @RequestParam("poblacion") String poblacion, @RequestParam("calle") String calle, @RequestParam("numero") int numero, 
+			@RequestParam("codigoPostal") int codigoPostal, @RequestParam("numCuenta") String numCuenta){
+		ComunidadDeVecinos cv = new ComunidadDeVecinos(cif, calle, numero, codigoPostal, numCuenta, poblacion);
+		comunidadDeVecinosRepository.save(cv);
+		
+		return "services";
 	}
-	
+	@RequestMapping(value="/uploadProperty", method=RequestMethod.POST)
+	public String addPropiedad(Model model,@RequestParam("propietarioSeleccionado") String p, @RequestParam("cif") String cif, @RequestParam("portalPropiedad") String portalPropiedad, @RequestParam("planta") String planta, 
+			@RequestParam("letra") char letra, @RequestParam("nombreProp") String nombreProp, @RequestParam("apellidosProp") String apellidosProp, @RequestParam("dni") String dni, 
+			@RequestParam("telf") int telefonoProp, @RequestParam("calleProp") String calleProp, @RequestParam("portalProp") int portalProp, @RequestParam("plantaProp") int plantaProp, 
+			@RequestParam("letraProp") char letraProp, @RequestParam("numCuentaProp") String numCuentaProp, @RequestParam("porcentaje") double porcentaje){
+		
+		String[] s= p.split(" ");
+		System.out.println(cif);
+		ComunidadDeVecinos cv= comunidadDeVecinosRepository.findBycifComunidadVecinos(cif);
+		Propietario p1;
+		if(nombreProp.equals("")&&apellidosProp.equals("")&&dni.equals("")){
+			p1= propietarioRepository.findBydniPropietario(s[s.length-1]);
+		}else{
+			p1 = new Propietario(nombreProp, apellidosProp, dni, telefonoProp, porcentaje, numCuentaProp);
+			propietarioRepository.save(p1);
+			Direccion dir = new Direccion(calleProp, portalProp, plantaProp, letraProp);
+			p1.setDireccionPropietario(dir);
+			direccionRepository.save(dir);			
+		}
+		Propiedad pd1= new Propiedad(portalPropiedad, planta, letra);
+		p1.addPropiedad(pd1);
+		cv.addPropiedad(pd1);
+		return "services";
+	}
 }
--- fincasSD/src/main/resources/static/js/javascript.js
+++ fincasSD/src/main/resources/static/js/javascript.js
@@ -4,6 +4,10 @@ $(function () {
         $('.bottonSavePos').css("visibility","visible");
     });
     
+    $('.verAniadirPropietario').click(function (){
+    	$('.newPropietario').css("visibility","visible");
+    });
+    
     $('.bottonSavePos').click(function () {
         $('.input').attr("readonly","readonly");
         $('.rightAlign').css("visibility","hidden");
