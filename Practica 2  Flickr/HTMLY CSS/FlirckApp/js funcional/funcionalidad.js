7ea1fe3c0a42e8e612a28b1071765febf99ef4e6
helogargon <helogargon@gmail.com>
2017-04-27 16:56:37 +0200

    muestra imagenes iniciales de flirck

--- Practica 2  Flickr/HTMLY CSS/FlirckApp/js funcional/funcionalidad.js
+++ Practica 2  Flickr/HTMLY CSS/FlirckApp/js funcional/funcionalidad.js
@@ -1,3 +1,34 @@
+function getHtml(url_img, msg) {
+    var html = "<li class=\"masonry-item grid\">" + "\n"
+                + "<figure class=\"effect-sarah\">" + "\n"
+                    + "<img src= \"" + url_img + "\"alt=\"\" />" + "\n"
+                        + "<figcaption>" + "\n"
+                            + "<h2>" + msg.substring(0, 30) + " ..." + "</h2>" + "\n"
+                            + "<a href=\"details.html\">View more</a>" + "\n"
+                        + "</figcaption>" + "\n"
+                + "</figure>" + "\n"
+            + "</li>";
+    return html;
+}
+
+
+$(document).ready(function () {
+    
+    var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + api_key + "&text=one+piece+anime+luffy+funny" + "&format=json&nojsoncallback=1";
+    
+    $.getJSON(url, function (data) {
+        console.log("OK");
+        $.each(data.photos.photo, function (i, photo) {
+            var msg = photo.title;
+            var url_img = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_h.jpg";
+            var html = getHtml(url_img, msg);
+            
+            $('#muro').append(html);
+        });
+    });
+    
+});
+
 $(function () {
     
     var textoABuscar = "";
@@ -5,12 +36,13 @@ $(function () {
     $('#enviarBoton').click(function () {
         textoABuscar = $('#textoRecoger').val();
         console.log(textoABuscar);
-    });
-    /**
-    $.getJSON(url, descripcion,
-        function(data) {
+        
+        
+        /**$.getJSON(url, descripcion,
+            function(data) {
 
-     });
-     **/
+         });**/
+     
+    });
 
 });
