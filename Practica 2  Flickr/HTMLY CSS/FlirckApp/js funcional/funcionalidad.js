a350785433879e5acf4ba822395b2260caf5932b
helogargon <helogargon@gmail.com>
2017-05-05 19:34:19 +0200

    ¿Buscáis problemas?...Pues escuchad nuestro lema...

--- Practica 2  Flickr/HTMLY CSS/FlirckApp/js funcional/funcionalidad.js
+++ Practica 2  Flickr/HTMLY CSS/FlirckApp/js funcional/funcionalidad.js
@@ -1,5 +1,6 @@
+var textoABuscar;
 function getHtml(url_img, msg, id, secret) {
-    var html = "<li class=\"masonry-item grid\">" + "\n"
+    var html = "<li class=\"masonry-item grid foto\">" + "\n"
                 + "<input type=\"checkbox\" class=\"listItem\" name=\"lista\">" + "\n"
                 + "<figure class=\"effect-sarah\">" + "\n"
                     + "<img class=\"listImage\" src= \"" + url_img + "\"alt=\"\" />" + "\n"
@@ -12,7 +13,7 @@ function getHtml(url_img, msg, id, secret) {
     return html;
 }
 
-$()
+
 function content(url_img, id, secret) {
     var url = "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=" + api_key + "&photo_id=" + id + "&secret=" + secret +  "&format=json&nojsoncallback=1";
     $.getJSON(url, function (data) {
@@ -21,32 +22,41 @@ function content(url_img, id, secret) {
     });
 }
 
-$(document).ready(function () {
-    var textoABuscar;
-    var url;
-    if (!textoABuscar) {  
-        url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + api_key + "&text=" + "flowers" + "&format=json&nojsoncallback=1";
-    } else {
-        console.log(textoABuscar);
-        url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + api_key + "&text=" + textoABuscar + "&format=json&nojsoncallback=1";
-    }
+function searchPhotos() {
+        console.log("Dentro");
+        
+        $(".foto").remove();
     
+        var textoABuscar = $('#textoRecoger').val();
+        
+        console.log(textoABuscar);
+    
+        api(textoABuscar);
+        
+}
+
+function api(textoABuscar){
+       
+    var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + api_key + "&text=" + textoABuscar + "&format=json&nojsoncallback=1";
     $.getJSON(url, function (data) {
-        console.log("OK");
         $.each(data.photos.photo, function (i, photo) {
             var msg = photo.title;
-            var id= photo.id;
-            var secret= photo.secret;
+            var id = photo.id;
+            var secret = photo.secret;
             var url_img = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_h.jpg";
-            var html = getHtml(url_img, msg,id,secret);
+            var html = getHtml(url_img, msg, id, secret);
             
             $('#muro').append(html);
         });
     });
-    
-    $('#enviarBoton').click(function () {
-        textoABuscar = $('#textoRecoger').val();
+}
 
+$(document).ready(function () {
+    
+    api("flowers");
+    
+     $('#enviarBoton').click(function (event) {
+            event.preventDefault();
     });
 
 });
