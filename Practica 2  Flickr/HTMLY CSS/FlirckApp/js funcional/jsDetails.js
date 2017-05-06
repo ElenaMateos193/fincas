c0dd22ec19c2e9b23bb2c4fe12b9a5b692f0c91d
helogargon <helogargon@gmail.com>
2017-05-06 20:52:34 +0200

    Para denunciar a los enemigos de la verdad y el amor.

--- Practica 2  Flickr/HTMLY CSS/FlirckApp/js funcional/jsDetails.js
+++ Practica 2  Flickr/HTMLY CSS/FlirckApp/js funcional/jsDetails.js
@@ -5,18 +5,26 @@ $(document).ready(function () {
     var id = arrVariables[0].substring(6, arrVariables[0].length);
     var secret = arrVariables[1].substring(13, arrVariables[1].length);
     
-    console.log(id + " ");
-    console.log(secret);
     
-
     var url = "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=" + api_key + "&photo_id=" + id + "&secret=" + secret + "&format=json&nojsoncallback=1";
 
  
     $.getJSON(url, function (data) {
-        console.log("OK");
-        console.log(data.photos);
-        /*$.each(data.photos.photo, function (i, photo) {
-
+        var url_img = "https://farm" + data.photo.farm + ".staticflickr.com/" + data.photo.server + "/" + data.photo.id + "_" + data.photo.secret + "_h.jpg";
+        
+        $('#selectedImg').append("<img src=\"" + url_img + "\"  class=\"img-responsive\" />");
+        $('#date').append(data.photo.dates.taken);
+        $('#realName').append(data.photo.owner.realname);
+        $('#alias').append("Alias: "+data.photo.owner.path_alias);
+        $('#title').append("<p>" + data.photo.title._content + "</p>");
+    });
+    
+    url="https://api.flickr.com/services/rest/?method=flickr.photos.getAllContexts&api_key=" + api_key +"&photo_id=" + id + "&format=json&nojsoncallback=1";
+    
+    $.getJSON(url, function (data) {
+        /*$.each(data, function (i, data) {            
+            console.log(data.pool);
+            //$('#sets').append("<a href=\"#\" class=\"list-group-item\">" + set.title + "</a>");
         });*/
     });
 });
