2800b7d1cca4433f35005e06180cdcf312de584b
helogargon <helogargon@gmail.com>
2017-05-08 16:20:40 +0200

    mono página

--- Practica 2  Flickr/HTMLY CSS/FlirckApp/js funcional/funcionalidad.js
+++ Practica 2  Flickr/HTMLY CSS/FlirckApp/js funcional/funcionalidad.js
@@ -1,54 +1,49 @@
+var id;
+var secret;
+var url_img;
 
 function getHtmlInit(url_img, msg, id, secret) {
-    var html = "<li class=\"masonry-item grid foto\">" + "\n"
-                + "<div class=\"listItem\">" +"\n"
-                    + "<input type=\"checkbox\" name=\"lista\">" + "\n"
-                    + "<figure class=\"effect-sarah\">" + "\n"
-                        + "<img class=\"listImage\" src= \"" + url_img + "\"alt=\"\" />" + "\n"
-                        + "<button id=\"#popUpImagen\" onclick=\"addPopUp("+ url_img + ");\" type=\"button\" data-toggle=\"modal\" data-target=\"#myModal\">Ver Imagen</button>" + "\n"
-                    + "</figure>" + "\n"
-                + "</div>" + "\n"
-            + "</li>";
+    var html = "<li class=\"masonry-item grid foto\">" + "\n" +
+        "<div class=\"listItem\">" + "\n" +
+        "<input type=\"checkbox\" name=\"lista\">" + "\n" +
+        "<figure class=\"effect-sarah\">" + "\n" +
+        "<img class=\"listImage\" src= \"" + url_img + "\"alt=\"\" />" + "\n" +
+        "<figcaption>" + "\n" +
+        "<h2>" + msg.substring(0, 30) + " ..." + "</h2>" + "\n" +
+        "<a id=\"enlace\" onclick=\"showDetails(\'" + id + "\', \'" + secret + "\');\">View more</a>" + "\n" +
+        "</figcaption>" + "\n" +
+        "</figure>" + "\n" +
+        "</div>" + "\n" +
+        "</li>";
     return html;
 }
-function addPopUp(url){
-    console.log("Entre en el popup");
-    var imagen = "<img src=\"" + url + "\"/>";
-    $("#imagenGrande").append(imagen);
-}
-function addPopUp2(url){
-    console.log("Entre en el popup");
-    var imagen = "<img src=\"" + url + "\"/>";
-    $("#imagenGrande2").append(imagen);
-}
 $(document).ready(function () {
-    
-     var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + api_key + "&text=" + "flowers" + "&format=json&nojsoncallback=1";
+
+    var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + api_key + "&text=" + "flowers" + "&format=json&nojsoncallback=1";
     $.getJSON(url, function (data) {
         $.each(data.photos.photo, function (i, photo) {
             var msg = photo.title;
-            var id = photo.id;
-            var secret = photo.secret;
-            var url_img = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_h.jpg";
+            id = photo.id;
+            secret = photo.secret;
+            url_img = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_h.jpg";
             var html = getHtmlInit(url_img, msg, id, secret);
             
             $('#muro').append(html);
         });
     });
-    
-     $('#enviarBoton').click(function (event) {
-            event.preventDefault();
-    });
-    
-     $('#enlace').click(function (event) {
+
+    $('#enviarBoton').click(function (event) {
         event.preventDefault();
-         console.log("se ejecuta");
+    });
+
+    $('#enlace').click(function (event) {
+        event.preventDefault();
     });
     $('#add-images').click(function (e) {
-        e.preventDefault();   
+        e.preventDefault();
     });
-    $('#popUpImagen').click(function (e) {
-        e.preventDefault();   
+    $('#back').click(function (event) {
+        event.preventDefault();
     });
 
 });
--- Practica 2  Flickr/HTMLY CSS/FlirckApp/js funcional/jsPhotos.js
+++ Practica 2  Flickr/HTMLY CSS/FlirckApp/js funcional/jsPhotos.js
@@ -1,7 +1,5 @@
 var textoABuscar;
-var id;
-var url_img;
-var secret;
+var text;
 
 function getHtml(url_img, msg, id, secret) {
     var html = "<li class=\"masonry-item grid foto\">" + "\n" +
@@ -10,27 +8,125 @@ function getHtml(url_img, msg, id, secret) {
         "<img class=\"listImage\" src= \"" + url_img + "\"alt=\"\" />" + "\n" +
         "<figcaption>" + "\n" +
         "<h2>" + msg.substring(0, 30) + " ..." + "</h2>" + "\n" +
-        "<a href=\"javascript:pasarParam('details.html', '" + "id, " + id + ", " + "secret, " + secret + "')\">View more</a>" + "\n" +
+        "<a id=\"enlace\" onclick=\"showDetails(\'" + id + "\', \'" + secret + "\');\">View more</a>" + "\n" +
         "</figcaption>" + "\n" +
         "</figure>" + "\n" +
         "</li>";
     return html;
 }
 
-function pasarParam(pagina, nombres) {
-    var i = 0;
-    pagina += "?";
-    var nomVec = nombres.split(",");
-    while (i < nomVec.length) {
-        pagina += nomVec[i] + "=" + nomVec[i + 1] + "&";
-        i = i + 2;
-    }
-    pagina = pagina.substring(0, pagina.length - 1);
-    location.href = pagina;
+function getHtmlDetails() {
+    var html = "<div id=\"destroy\">"+
+        "<button id=\"back\" type=\"submit\" onclick=\"restaurar();\">Atrás</button>" +
+        "<div class=\"wrapper-inner\">" +
+        "<figure class=\"details-image\" id=\"selectedImg\">" +
+        "</figure>" +
+        "<div class=\"details-content\">" +
+        "<section class=\"inner-left\">" +
+        "<header>" +
+        "<h3 id=\"date\"></h3>" +
+        "<h4 id=\"realName\"></h4>" +
+        "<h5 id=\"alias\"></h5>" +
+        "</header>" +
+        "</section>" +
+        "<section class=\"inner-right\" id=\"title\">" +
+        "</section>" +
+        "</div>" +
+        "<div class=\"panel panel-default\">" +
+        "<div class=\"panel-heading\">Albums</div>" +
+        "<div class=\"panel-body\">" +
+        "<div class=\"list-group\" id=\"sets\"></div>" +
+        "</div>" +
+        "</div>" +
+        "<div class=\"panel panel-default\">" +
+        "<div class=\"panel-heading\">Grupos</div>" +
+        "<div class=\"panel-body\">" +
+        "<div class=\"list-group\" id=\"pools\">" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "<div class=\"panel panel-default\">" +
+        "<div class=\"panel-heading\">Galerías</div>" +
+        "<div class=\"panel-body\">" +
+        "<div class=\"list-group\" id=\"galleries\"></div>" +
+        "</div>" +
+        "</div>" +
+        "</div>"+
+        "</div>";
 
+    return html;
 }
 
+function restaurar() {
+    if(text!== undefined){
+        searchPhotosAux(text);
+    }else{
+        searchPhotosAux("flowers");
+    }
+}
 
+function saveText(textx){
+    text=textx;
+}
+
+function showDetails(id, secret) {
+
+    $(".foto").remove();
+    $('#destroy').remove();
+
+    var html = getHtmlDetails();
+
+    $('#details').append(html);
+
+    chargeDetails(id, secret);
+}
+
+function chargeDetails(id, secret) {
+
+    var url = "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=" + api_key + "&photo_id=" + id + "&secret=" + secret + "&format=json&nojsoncallback=1";
+
+
+    $.getJSON(url, function (data) {
+        var url_img = "https://farm" + data.photo.farm + ".staticflickr.com/" + data.photo.server + "/" + data.photo.id + "_" + data.photo.secret + "_h.jpg";
+
+        $('#selectedImg').append("<img src=\"" + url_img + "\"  class=\"img-responsive\" />");
+        $('#date').append(data.photo.dates.taken);
+        $('#realName').append(data.photo.owner.realname);
+        $('#alias').append("Alias: " + data.photo.owner.path_alias);
+        $('#title').append("<p>" + data.photo.title._content + "</p>");
+    });
+
+    var url_context = "https://api.flickr.com/services/rest/?method=flickr.photos.getAllContexts&api_key=" + api_key + "&photo_id=" + id + "&format=json&nojsoncallback=1";
+
+    $.getJSON(url_context, function (data) {
+        if (data.set) {
+            $.each(data.set, function (i, set) {
+                $('#sets').append("<a href=\"#\" class=\"list-group-item\">" + set.title + "</a>");
+            });
+        } else {
+            $('#sets').append("<a href=\"#\" class=\"list-group-item\">No está en ningún album</a>");
+        }
+        if (data.pool) {
+            $.each(data.pool, function (i, pool) {
+                $('#pools').append("<a href=\"#\" class=\"list-group-item\">" + pool.title + "</a>");
+            });
+        } else {
+            $('#pools').append("<a href=\"#\" class=\"list-group-item\">No está en ningún grupo</a>");
+        }
+    });
+
+    var url_galleries = "https://api.flickr.com/services/rest/?method=flickr.galleries.getListForPhoto&api_key=" + api_key + "&photo_id=" + id + "&format=json&nojsoncallback=1";
+
+    $.getJSON(url_galleries, function (data) {
+        if (data.galleries.total != 0) {
+            $.each(data.galleries.gallery, function (i, gallery) {
+                $('#galleries').append("<a href=\"#\" class=\"list-group-item\">" + gallery.title._content + "</a>");
+            });
+        } else {
+            $('#galleries').append("<a href=\"#\" class=\"list-group-item\">No está en ninguna galleria</a>");
+        }
+    });
+}
 
 function searchPhotos() {
 
@@ -42,7 +138,15 @@ function searchPhotos() {
 
 }
 
+function searchPhotosAux(text){
+    $('#destroy').remove();
+
+    apiPhotos(text);
+}
+
 function apiPhotos(textoABuscar) {
+    
+    saveText(textoABuscar);
 
     var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + api_key + "&text=" + textoABuscar + "&format=json&nojsoncallback=1";
 
@@ -53,7 +157,7 @@ function apiPhotos(textoABuscar) {
             secret = photo.secret;
             url_img = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_h.jpg";
             var html = getHtml(url_img, msg, id, secret);
-            
+
             $('#muro').append(html);
         });
     });
